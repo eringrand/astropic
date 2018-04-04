b4a02f3112dfb21649e7960b929eeb5eba47d6e5
eringrand <eringrand@gmail.com>
2018-04-04 18:19:59 -0400

    documentation for the data files

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Historical APOD image data from 2007 - 2017
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{copyright}{imge copyright, if not public domain}
+#'   \item{date}{date of image on APOD}
+#'   \item{explanation}{explanation of image, may have some HTML text still in it}
+#'   \item{hdurl}{high-def image url}
+#'   \item{media_type}{type of media, image or video}
+#'   \item{service_version}{unknown}
+#'   \item{title}{title of image}
+#'   \item{url}{url of image}
+#' }
+"hist_apod"
