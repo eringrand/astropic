a16701396d39e08f48c8f0f059b39557dbec3519
eringrand <eringrand@gmail.com>
2018-04-04 19:57:29 -0400

    not much changes

--- R/apodR.R
+++ R/apodR.R
@@ -4,6 +4,6 @@
 #' @importFrom httr GET content headers
 #' @keywords internal
 NULL
-## quiets concerns of R CMD check re: the .'s that appear in pipelines
-## and the "n" that is produced by dplyr::count() in a pipeline
+#' quiets concerns of R CMD check re: the .'s that appear in pipelines
+#' and the "n" that is produced by dplyr::count() in a pipeline
 if(getRversion() >= "2.15.1") utils::globalVariables(c(".", "n"))
--- R/data.R
+++ R/data.R
@@ -3,12 +3,12 @@
 #' @format A data frame with 53940 rows and 10 variables:
 #' \describe{
 #'   \item{copyright}{imge copyright, if not public domain}
-#'   \item{date}{date of image on APOD}
-#'   \item{explanation}{explanation of image, may have some HTML text still in it}
-#'   \item{hdurl}{high-def image url}
-#'   \item{media_type}{type of media, image or video}
+#'   \item{date}{Date of the image}
+#'   \item{explanation}{The supplied text explanation of the image, may have some HTML text still in it}
+#'   \item{hdurl}{The url for the high-resolution image}
+#'   \item{media_type}{The type of media returned. May either be 'image' or 'video' depending on content}
 #'   \item{service_version}{unknown}
-#'   \item{title}{title of image}
-#'   \item{url}{url of image}
+#'   \item{title}{The title of image}
+#'   \item{url}{The url of image}
 #' }
 "hist_apod"
