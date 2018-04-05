3a36c7a2abe7efe3cd669ea9f83615d2200f4638
eringrand <eringrand@gmail.com>
2018-04-04 20:29:49 -0400

    change of name

--- R/astropic.R
+++ R/astropic.R
@@ -1,9 +1,8 @@
 #' @docType package
-#' @name apodR
+#' @name astropic
 #' @importFrom dplyr %>%
 #' @importFrom httr GET content headers
 #' @keywords internal
 NULL
 #' quiets concerns of R CMD check re: the .'s that appear in pipelines
-#' and the "n" that is produced by dplyr::count() in a pipeline
-if(getRversion() >= "2.15.1") utils::globalVariables(c(".", "n"))
+if(getRversion() >= "2.15.1") utils::globalVariables(c("."))
