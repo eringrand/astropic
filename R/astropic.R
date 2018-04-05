0fa3d4709778d0f292b0e2216c6ba60b36c733c6
eringrand <eringrand@gmail.com>
2018-04-04 20:48:46 -0400

    change name

--- R/astropic.R
+++ R/astropic.R
@@ -1,7 +1,6 @@
 #' @docType package
 #' @name astropic
-#' @importFrom dplyr %>%
-#' @importFrom httr GET content headers
+#' @importFrom purrr %>%
 #' @keywords internal
 NULL
 #' quiets concerns of R CMD check re: the .'s that appear in pipelines
