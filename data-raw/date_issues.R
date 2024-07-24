58ee5cbf9e5a223b7b411df5ad684b9e582f78df
eringrand <eringrand@gmail.com>
2024-07-23 23:57:22 -0400

    Update checking for data issues file to new years - hopefully lots of issues are fixed!

--- data-raw/date_issues.R
+++ data-raw/date_issues.R
@@ -1,26 +1,47 @@
-# FInd all dates with issues - rest for an hour im between 900 requests
+# Find all dates with issues - rest for an hour im between 1900 requests
 
+# Setup ---------------------------------------------------------------------------------------------------------------------------------------------------
+
+library(astropic)
 library(ratelimitr)
 library(lubridate)
 library(purrr)
 
+
+# Write Funtion -------------------------------------------------------------------------------------------------------------------------------------------------
+
+
 try_date <- function(date_string) {
+
   url <- astropic:::make_url(query = list(date = date_string))
   rsp <- httr::GET(url)
   code <- httr::status_code(rsp)
-  limit <- astropic:::rate_limit(rsp) #  Hourly Limit: 1,000 requests per hour
+  limit <- astropic:::rate_limit(rsp) #  Hourly Limit: 2,000 requests per hour
+
+  print(limit)
+
   if(code == 500) {
     return(date_string)
   }
+
   else{
     return(NA_character_)
   }
 }
 
-try_date_lim <- limit_rate(try_date, rate(n = 999, period = 3600))
+
+# Find missing dates --------------------------------------------------------------------------------------------------------------------------------------
+
+try_date_lim <- limit_rate(try_date, rate(n = 1940, period = 3600))
 
 
-dates <- seq(ymd("2007-01-01"), ymd("2019-12-31"), by = "days") %>%
+dates <- seq(ymd("2007-01-01"), ymd("2024-01-31"), by = "days") %>%
   as.character()
 
 dates_w_issues <- map_chr(dates, try_date_lim)
+dates_w_issues <- unique(dates_w_issues)[complete.cases(unique(dates_w_issues))]
+
+# Output dates --------------------------------------------------------------------------------------------------------------------------------------
+
+usethis::use_data(dates_w_issues, overwrite = TRUE)
+
