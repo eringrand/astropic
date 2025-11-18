75cc23b700bab0cf8189a4dfc49baac048433c7a
eringrand <eringrand@gmail.com>
2025-11-18 17:50:07 -0500

    add prefix functions to scripts

--- data-raw/date_issues.R
+++ data-raw/date_issues.R
@@ -1,14 +1,9 @@
-# Find all dates with issues - rest for an hour in between 900 requests
-library(ratelimitr)
-library(lubridate)
-library(purrr)
-library(astropic)
-
+# Find all dates with issues - rest for an hour in between 1900 requests
 try_date <- function(date_string) {
-  url <- make_url(query = list(date = date_string))
+  url <- astropic:::make_url(query = list(date = date_string))
   rsp <- httr::GET(url)
   code <- httr::status_code(rsp)
-  limit <- rate_limit(rsp)
+  limit <- astropic:::rate_limit(rsp)
   if(code == 500) {
     return(date_string)
   }
@@ -17,10 +12,10 @@ try_date <- function(date_string) {
   }
 }
 
-try_date_lim <- limit_rate(try_date, rate(n = 999, period = 3600))
+try_date_lim <- ratelimitr::limit_rate(try_date, rate(n = 1999, period = 3600))
 
-dates <- seq(ymd("2007-01-01"), today(), by = "days") |>
+dates <- seq(lubridate::ymd("2007-01-01"), lubridate::today(), by = "days") |>
   as.character()
 
-dates_w_issues <- map_chr(dates, try_date_lim)
+dates_w_issues <- purrr::map_chr(dates, try_date_lim)
 readr::write_rds(dates_w_issues, here::here("data-raw/dates_w_issues.csv"))
