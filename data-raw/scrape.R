ed39804cc02caa436547c2558fb2b2f7e229fcb0
eringrand <eringrand@gmail.com>
2025-11-23 19:54:42 -0500

    remove extra info

--- data-raw/scrape.R
+++ data-raw/scrape.R
@@ -3,21 +3,8 @@ library(lubridate)
 library(readr)
 
 # Set up what days we want data
-start_date <- ymd("2007-01-01")
 today <- today() - 1
-
-days_wanted <- seq(start_date, today, by = 1)
-
-date <- as.Date(today)
-print(date)
-
-if(file.exists(glue::glue("rds/{date}.csv"))) {
-    next
-}
-
 today_pic <- get_apod(query = list(date = today))
 
 # Write out data
 readr::write_rds(today_pic, glue::glue("data-raw/rds/{date}.rds"))
-
-
