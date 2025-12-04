e5d52791de5048a8d33041a28f1973dc9f9afe6d
eringrand <eringrand@gmail.com>
2025-12-03 23:34:59 -0500

    update data with daily downloads

--- data-raw/put_daily_download_together.R
+++ data-raw/put_daily_download_together.R
@@ -11,11 +11,16 @@ error_dates <- read_rds("data-raw/dates_w_issues.rds") |>
   mutate(year = year(ymd(date))) |> 
   filter(! is.na(date))
 
+data(hist_apod) 
+
 data_dates <- hist_apod |> 
   distinct(date) |> 
+  mutate(date = ymd(date)) |>
   pull(date)
 
-dates_with_img <- map_dfr(list.files("data-raw/rds", full.names = TRUE), read_rds) |>
+apod_daily <- map_dfr(list.files("data-raw/rds", full.names = TRUE), read_rds) 
+
+dates_with_img <- apod_daily |> 
   distinct(date) |>
   mutate(date = ymd(date)) |>
   pull(date) 
@@ -24,16 +29,23 @@ days_wanted <- seq(ymd("2007-01-01"), today(), by = 1)
 
 days_missing <- setdiff(days_wanted, dates_with_img) |>
   setdiff(ymd(pull(error_dates, date))) |>
+  setdiff(data_dates) |> 
   as.Date()
 
-days_missing
-
-hist_apod_extra <- map_dfr(list.files("data-raw/rds", full.names = TRUE), read_rds) |>
-  distinct()
+if(length(days_missing) > 0) {
+  days_missing_dat <- map_dfr(days_missing, ~get_apod(query = list(date = .x)))
+  
+  hist_apod <- hist_apod |> 
+    bind_rows(x) |> 
+    distinct()
+}
 
 hist_apod <- hist_apod |> 
-  bind_rows(hist_apod_extra) |> 
+  bind_rows(apod_daily) |> 
   distinct()
 
-# Push Data to Data/Folder
+hist_apod |>
+  janitor::get_dupes(date)
+
+# Push Data to Data/Fdate# Push Data to Data/Folder
 usethis::use_data(hist_apod, overwrite = TRUE)
